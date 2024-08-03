5932fcbdc83c13e9c865d2be0fe30c4af3d258b2
Devendra Parmar <23030501043@darshan.ac.in>
2024-08-03 16:31:59 +0530

    React Pokemon Cards Website Using API and With Search Functionality

---
+++ pokemon_react/src/Pokemon.jsx
@@ -0,0 +1,87 @@
+import { useEffect, useState } from "react";
+import "./index.css";
+import PokemonCards from "./PokemonCards";
+
+const Pokemon = () => {
+  const [pokemon, setPokemon] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [search, setSearch] = useState("");
+
+  const API = "https://pokeapi.co/api/v2/pokemon?limit=100";
+
+  const fetchPokemon = async () => {
+    try {
+      const res = await fetch(API);
+      const data = await res.json();
+
+      const detailedPokemonData = data.results.map(async (currPokemon) => {
+        const res = await fetch(currPokemon.url);
+        const data = await res.json();
+        return data;
+      });
+
+      const detailedResponses = await Promise.all(detailedPokemonData);
+      setPokemon(detailedResponses);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+      setError(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchPokemon();
+  }, []);
+
+  // Search Functionality
+  const searchData = pokemon.filter((currData) =>
+    currData.name.toLowerCase().includes(search.toLowerCase())
+  );
+
+  if (loading) {
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div>
+        <h1>Error: {error.message}</h1>
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <section className="container">
+        <header>
+          <h1>Lets Catch Pokemon</h1>
+        </header>
+        <div class="pokemon-search">
+          <input
+            type="text"
+            placeholder="Search Pokemon Here"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+        </div>
+        <div>
+          <ul className="cards">
+            {searchData.map((currPokemon) => {
+              return (
+                <PokemonCards key={currPokemon.id} pokemonData={currPokemon} />
+              );
+            })}
+          </ul>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Pokemon;
